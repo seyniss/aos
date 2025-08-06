7df0f3b2fb678b8ae7c97557c486cb2d29ef9943
seyniss <shepunk2@kbu.ac.kr>
2025-08-06 15:58:53 +0900

    11

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Hero from './components/Hero'
+import Contact from './components/Contact'
+import Aboutme from './components/Aboutme'
+import Skill from './components/Skill'
+function App() {
+
+
+  return (
+    <div>
+    <Hero/>
+    <Contact/>
+    <Aboutme/>
+    <Skill/>
+  
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style/Aboutme.css'
+const Aboutme = () => {
+  return (
+    <section>
+      <div className="t-wrap">
+        <h2>aboutme title</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, voluptas!
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Aboutme
